dd017074fd6e9ef45f4887efe901b3a724ffa020
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2021-02-28 14:33:26 -0700

    I have no idea what I'm doing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-// import React, { useState } from 'react';
-// import Header from './components/Header';
-// import Nav from './components/Nav';
-// import Project from './components/Project';
-// import ContactForm from './components/Contact';
-// import Footer from './components/Footer';
-
-
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Nav from './components/Nav';
+import Header from "./components/Header";
+import Project from "./components/Project";
+import About from "./components/About";
+import ContactForm from './components/Contact';
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <main>
+        <Project></Project>
+        <About></About>
+        <ContactForm></ContactForm>
+        <Footer></Footer>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react"
 
-function Header() {
-
+const Header = () => {
+  return (
+    <div className="section" id="home">
+      <div className="container">
+        <div className="header-wrapper">
+          <h2>
+            Hi, I'm Kristin
+          </h2>
+          <div className="heading-wrapper">
+            <h1>
+              Create Your Own Moonshine
+            </h1>
+          </div>
+          <p>I am a native Arizonan and currently live in Central Phoenix with my husband and two-year-old son. I love to travel the world and look forward to sharing that with my growing family. I have my degree in art history from the University of Arizona and have been an event planner for the past 15 years, coordinating large conferences, professional development trainings, fundraisers, golf tournaments, weddings, and art shows. Recently, I also completed my Master Florist certification through the Arizona Florists Association. Now, rounding out my resume, I am pursuing a certificate in coding through the University of Arizona. This is the beginning of my online portfolio. Enjoy!</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header;
