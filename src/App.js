4b945be711eebb507b9d9addf51de11aff7c5fe0
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2021-03-01 03:02:31 -0700

    side navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "materialize-css/dist/css/materialize.min.css";
 
+class App extends Component {
+  componentDidMount() {
+    let sidenav = document.querySelector('.sidenav');
+    M.Sidenav.init(sidenav, {});
+  }
 
-const App = () => {
-  return (
-    <div>
-      <Header></Header>
-      <Footer></Footer>
-    </div>
-  )
-};
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <Footer></Footer>
+      </div>
+    )
+  };
+}
 
 export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,9 +7,9 @@ function Nav(props) {
     <div className="navbar-fixed">
       <nav>
         <div className="nav-wrapper teal lighten-3">
-          <a href="#!" className="brand-logo">Moon Ink</a>
+          <a href="#about" className="brand-logo">Moon Ink</a>
           <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-          <ul id="nav-mobile" className="nav nav-tabs right hide-on-med-and-down">
+          <ul className="nav nav-tabs right hide-on-med-and-down">
             {tabs.map(tab => (
               <li className="nav-item" key={tab}>
                 <a
@@ -26,10 +26,24 @@ function Nav(props) {
           </ul>
         </div>
       </nav>
+
+      <ul className="sidenav" id="mobile-demo">
+        {tabs.map(tab => (
+          <li className="nav-item" key={tab}>
+            <a
+              href={'#' + tab.toLowerCase()}
+              onClick={() => props.handlePageChange(tab)}
+              className={
+                props.currentPage === tab ? 'nav-link active' : 'nav-link'
+              }
+            >
+              {tab}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-
 
 export default Nav;
